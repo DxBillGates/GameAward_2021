3e90151a74386687e6c51da28e0550cf0f2ee9cb
nayul <k019g1485@g.neec.ac.jp>
2021-04-20 13:59:18 +0900

    3rd

---
+++ Assets/line/BatGenerator2.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatGenerator2 : MonoBehaviour
+{
+
+    public GameObject target;
+    public GameObject bat;
+    public GameObject ebat;
+
+    GameObject b;
+    GameObject c;
+
+    int count = 0;
+    Vector3 sPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.Q) && count < 2)
+        {
+            count++;
+
+            if (count == 1)
+            {
+                b = Instantiate(bat) as GameObject;
+                b.transform.rotation = target.transform.rotation;
+                b.transform.position = target.transform.position;
+
+                if (target.transform.position.x >= 0)
+                {
+                    Vector3 pos = b.transform.position;
+                    pos.x -= 0;
+                    b.transform.position = pos;
+                }
+
+                sPosition = b.transform.position;
+            }
+
+            if (count >= 2)
+            {
+
+                c = Instantiate(ebat) as GameObject;
+                c.transform.rotation = target.transform.rotation;
+                c.transform.position = target.transform.position;
+
+                Coroutine cor = StartCoroutine("DelayMethod", 10);
+
+            }
+
+        }
+
+    }
+
+    private IEnumerator DelayMethod(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        GameObject[] cubes = GameObject.FindGameObjectsWithTag("box");
+        GameObject[] ends = GameObject.FindGameObjectsWithTag("endBox");
+
+        GameObject[] checks = GameObject.FindGameObjectsWithTag("checker");
+
+        foreach (GameObject check in checks)
+        {
+           
+            Destroy(check);
+        }
+
+        foreach (GameObject cube in cubes)
+        {
+           
+            Destroy(cube);
+        }
+
+        foreach (GameObject end in ends)
+        {
+            
+            Destroy(end);
+        }
+
+        count = 0;
+
+    }
+
+}
