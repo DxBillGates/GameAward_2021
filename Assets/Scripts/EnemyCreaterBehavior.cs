9cc273d0b4bc08e5de43b47349604535db3315fe
To_Mo_Pi <k019g1234@g.neec.ac.jp>
2021-04-21 14:30:32 +0900

    fixed CreatedEnemy's public member, add prefab enemy

--- Assets/Scripts/EnemyCreaterBehavior.cs
+++ Assets/Scripts/EnemyCreaterBehavior.cs
@@ -7,25 +7,22 @@ public class EnemyCreaterBehavior : MonoBehaviour
     public bool leftMode;
     public Vector3 fallVector;
     public bool drawRay;
-    public float timeSpan;
-    public int max;
+    public float createTimeSpan;
+    public int maxEnemy;
     public Mesh rightMesh;
     public Mesh setMesh;
     public Material setMaterial;
-
-    public float damageSpan;
-    public float hp;
     int count;
     float t;
-    public float attackTimeSpan;
+    public GameObject prefabEnemy;
     // Start is called before the first frame update
     void Start()
     {
         count = 0;
         t = 0;
-        if (timeSpan == 0)
+        if (createTimeSpan == 0)
         {
-            timeSpan = 3;
+            createTimeSpan = 3;
         }
 
         GetComponent<Renderer>().material.color = (leftMode) ? Color.blue : Color.red;
@@ -37,7 +34,7 @@ public class EnemyCreaterBehavior : MonoBehaviour
         if (drawRay)
             Debug.DrawRay(transform.position, fallVector * 10, Color.red);
 
-        if (t >= timeSpan && count < max)
+        if (t >= createTimeSpan && count < maxEnemy)
         {
             Create();
             count++;
@@ -49,54 +46,69 @@ public class EnemyCreaterBehavior : MonoBehaviour
 
     void Create()
     {
-        GameObject gameObject = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        //ラープポイントの取得用
         LerpBehaviour player = GameObject.Find("Player").GetComponent<LerpBehaviour>();
 
-        Destroy(gameObject.GetComponent<CapsuleCollider>());
-        gameObject.AddComponent<BoxCollider>();
+        //プレハブから追加する情報を取得
+        ObjectBehavior prefabObjBehavior = prefabEnemy.GetComponent<ObjectBehavior>();
+        LerpBehaviour prefabLerpBehavior = prefabEnemy.GetComponent<LerpBehaviour>();
+        EnemyBehaviour prefabEnemyBehavior = prefabEnemy.GetComponent<EnemyBehaviour>();
+        AttackBehaviour prefabAttackBehavior = prefabEnemy.GetComponent<AttackBehaviour>();
 
-        ObjectBehavior objBehavior = gameObject.AddComponent<ObjectBehavior>();
-        EnemyBehaviour enemyBehaviour = gameObject.AddComponent<EnemyBehaviour>();
-        enemyBehaviour.onMove = false;
-        LerpBehaviour lerpBehaviour = gameObject.AddComponent<LerpBehaviour>();
-        lerpBehaviour.frontList = player.frontList;
-        lerpBehaviour.backList = player.backList;
-        lerpBehaviour.isAutoMove = true;
+        //新規オブジェクトの生成
+        GameObject newGameObject = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        newGameObject.name = "CreatedEnemy";
 
-        lerpBehaviour.leftMove = leftMode;
+        //コライダーの切り替え
+        Destroy(newGameObject.GetComponent<Collider>());
+        newGameObject.AddComponent<BoxCollider>();
 
-        objBehavior.fallVector = fallVector.normalized / 100;
+        //ビヘイビアを追加＆取得
+        ObjectBehavior newObjectBehavior = newGameObject.AddComponent<ObjectBehavior>();
+        LerpBehaviour newLerpBehvior = newGameObject.AddComponent<LerpBehaviour>();
+        EnemyBehaviour newEnemyBehavior = newGameObject.AddComponent<EnemyBehaviour>();
+        AttackBehaviour newAttackBehavior = newGameObject.AddComponent<AttackBehaviour>();
 
-        gameObject.transform.position = transform.position;
-        gameObject.name = "CreatedEnemy";
-        gameObject.layer = 6;
+        //オブジェクトビヘイビアの設定
+        newObjectBehavior.fallVector = fallVector.normalized / 10;
 
-        gameObject.transform.rotation = Quaternion.FromToRotation(gameObject.transform.up, -fallVector) * gameObject.transform.rotation;
-        gameObject.transform.rotation = Quaternion.FromToRotation(gameObject.transform.right, -gameObject.transform.right) * gameObject.transform.rotation;
+        //ラープビヘイビアの設定
+        newLerpBehvior.frontList = player.frontList;
+        newLerpBehvior.backList = player.backList;
+        newLerpBehvior.isAutoMove = true;
+        newLerpBehvior.leftMove = leftMode;
 
-        gameObject.tag = "enemy";
+        //エネミービヘイビアの設定
+        newEnemyBehavior.onMove = false;
+        newEnemyBehavior.damageSpan = prefabEnemyBehavior.damageSpan;
+        newEnemyBehavior.hp = prefabEnemyBehavior.hp;
 
-        AttackBehaviour atk = gameObject.AddComponent<AttackBehaviour>();
-        MeshFilter mf = gameObject.GetComponent<MeshFilter>();
-        MeshRenderer mr = gameObject.GetComponent<MeshRenderer>();
+        //アタックビヘイビアの設定
+        newAttackBehavior.timeSpan = prefabAttackBehavior.timeSpan;
+
+        //生成オブジェクトの詳細設定
+        newGameObject.transform.position = transform.position;
+        newGameObject.layer = 6;
+        newGameObject.transform.rotation = Quaternion.FromToRotation(newGameObject.transform.up, -fallVector) * newGameObject.transform.rotation;
+        newGameObject.transform.rotation = Quaternion.FromToRotation(newGameObject.transform.right, -newGameObject.transform.right) * newGameObject.transform.rotation;
+
+        //モデル設定
+        MeshFilter newMeshFilter = newGameObject.GetComponent<MeshFilter>();
+        MeshRenderer newMeshRenderer = newGameObject.GetComponent<MeshRenderer>();
         if (setMesh)
         {
             if (!leftMode)
             {
-                mf.mesh = rightMesh;
+                newMeshFilter.mesh = rightMesh;
             }
             else
             {
-                mf.mesh = setMesh;
+                newMeshFilter.mesh = setMesh;
             }
         }
         if (setMaterial)
         {
-            mr.material = setMaterial;
+            newMeshRenderer.material = setMaterial;
         }
-        enemyBehaviour.damageSpan = damageSpan;
-        enemyBehaviour.hp = hp;
-        atk.timeSpan = attackTimeSpan;
-
     }
 }
