b740563f003889bb0034466e7f2bad313f609354
To_Mo_Pi <k019g1234@g.neec.ac.jp>
2021-05-28 18:18:29 +0900

    add clear effect

--- Assets/Scripts/GameClearBehaviour.cs
+++ Assets/Scripts/GameClearBehaviour.cs
@@ -11,23 +11,68 @@ public class GameClearBehaviour : MonoBehaviour
     //[SerializeField] GameObject pig;
     //[SerializeField] GameObject gorilla;
     // Start is called before the first frame update
+    AudioManagerBehaviour audioManager;
+    bool isBreak;
+    float time;
+    int i;
+    List<GameObject> gameObjects;
+    public RectTransform gameClearUI;
+    Vector3 initialRotate;
+    Vector3 initialScale;
+    float lerpTime;
     void Start()
     {
         FadeManager2.FadeIn();
-        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("enemy");
-        foreach (var obj in gameObjects)
-        {
-            EnemyCheck enemyCheck = obj.GetComponent<EnemyCheck>();
-            enemyCheck.BreakPolygon(true);
-        }
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManagerBehaviour>();
+        isBreak = false;
+        time = 0;
+        i = 0;
+        gameObjects = new List<GameObject>();
+
+        initialRotate = gameClearUI.localEulerAngles;
+        initialScale = gameClearUI.localScale;
+        audioManager.clearSE.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isBreak && time >= 0.8f)
+        {
+            GameObject[] objects = GameObject.FindGameObjectsWithTag("enemy");
+            foreach (var g in objects)
+            {
+                gameObjects.Add(g);
+            }
+            isBreak = true;
+            time = 0;
+        }
+
+        if (isBreak && time >= 0.2f && i < gameObjects.Count && !audioManager.clearSE.isPlaying)
+        {
+            EnemyCheck e = gameObjects[i].GetComponent<EnemyCheck>();
+            if (e) e.BreakPolygon(true);
+            audioManager.glassSE.Play();
+            ++i;
+            time = 0;
+        }
+
+        if (lerpTime >= 1) lerpTime = 1;
+
+        gameClearUI.transform.localEulerAngles = Vector3.Lerp(initialRotate, new Vector3(0, 360, 0), easeInOutQuint(lerpTime));
+        gameClearUI.transform.localScale = Vector3.Lerp(initialScale, new Vector3(2, 2, 2), easeInOutQuint(lerpTime));
+
+        lerpTime += Time.deltaTime * 0.5f;
+        time += Time.deltaTime;
+
         if (Input.GetMouseButtonDown(0))
         {
             FadeManager2.FadeOut("StageSelect");
         }
+    }
+
+    float easeInOutQuint(float x)
+    {
+        return x < 0.5 ? 16 * x * x * x * x * x : 1 - Mathf.Pow(-2 * x + 2, 5) / 2;
     }
 }
